f8421a63d184b8db30a08a8cc4032eade3cafb54
hassanmomoh <163112395+hassanmomoh@users.noreply.github.com>
2024-11-26 11:02:58 +0100

    format landing page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -7,12 +7,11 @@ import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 <template>
     <GuestLayout >
-        <h1 class="font-poppins text-xl uppercase"> MIMS: Meter Installation Management Software.</h1>
-       <p class="font-poppins text-lg mb-8">
+        <h1 class="font-poppins font-extrabold text-xl uppercase"> MIMS: Meter Installation Management Software.</h1>
           
-<strong class="text-justify">
+<h3 class="text-justify font-poppins">
     Manages installation, installers, staff and Inventory. <br>
-It automates installation processes starting from schedule, to requesting for meter by team lead to capturing which meter is install for a particular customer, when was it installed.
+It automates installation processes starting from schedule, to requesting for meter by team lead to capturing which meter is install for a particular customer and when.
 <!--The modules are. -->
 <!-- <li><strong>Inventory</strong>: This keeps track of every installation item in a particular region with the help of the store manager.</li>
 <li><strong>Schedule</strong>: This module provides an interface through which we upload a list of customers to be installed in a particular region as provided by Disco (JED).</li>
@@ -21,19 +20,18 @@ It automates installation processes starting from schedule, to requesting for me
 <li><strong>Customer Complaints</strong>: Keeps track of customers' complaints and resolutions.</li>
 <li><strong>Report</strong>: A comprehensive report interface that summarizes activities of each region using charts and tables for management decisions.</li>
 -->
-</strong> 
+</h3> 
 
 
 
-       </p>
-       <h1 class="font-poppins text-xl mb-5 text-center">PARTNERS</h1>
+       <h1 class="font-poppins text-2xl mb-3 text-center">PARTNERS</h1>
        <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
         <div class="px-2">
-            <h2 class="uppercase text-xl font-extrabold"> Technovati ltd</h2>
+            <h2 class="uppercase text-xl font-extrabold"> Technical Partner</h2>
             <div class=" text-center">
-                <img src="/files/images/technovati.png" alt="Smart Meter" class="w-[250px]">
+                <img src="/files/images/tvati logo.png" alt="Smart Meter" class="w-[250px]">
             </div>
-            <p class="text-justify">
+            <p class="text-justify font-poppins">
                 Technovati meters are equipped with 4G GPRS Modems with fall back capability to 2G and engineered with the latest technology in Advanced metering 
                 infrastructure that enables remote monitoring of consumption, tamper detection, event sensing and triggers.
             </p>
@@ -41,11 +39,11 @@ It automates installation processes starting from schedule, to requesting for me
         </div>
 
         <div class="px-2">
-            <h2 class="uppercase text-xl font-extrabold">JED PLC</h2>
+            <h2 class="uppercase text-xl font-extrabold">CLIENT</h2>
             <div class=" text-center">
-                <img src="/files/images/jed logo.png" alt="Smart Meter" class="w-[100px]  text-center">
+                <img src="/files/images/jed.png" alt="Smart Meter" class="w-[150px]  text-center">
             </div>
-            <p class="text-justify">
+            <p class="text-justify font-poppins">
                 JED PLC is an indigenous electricity Company, incorporated in Nigeria to carry out the business of electricity distribution and retail sale in the franchise states of Benue, Plateau, Bauchi and Gombe. We pride ourselves on delivering quality electricity to our esteemed customers.
             </p>
         </div>
