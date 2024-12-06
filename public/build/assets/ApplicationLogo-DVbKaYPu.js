cc0dc4d6a6bf770d81a5f072403482f5f9afa8e1
hassanmomoh <163112395+hassanmomoh@users.noreply.github.com>
2024-12-06 01:21:25 +0100

    bring back dt name

--- public/build/assets/ApplicationLogo-DVbKaYPu.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c}from"./app-9XFmDN4-.js";const t={},n={src:"/files/images/logo.png",alt:"Triple Seventh Logo"};function s(r,_){return e(),c("img",n)}const i=o(t,[["render",s]]);export{i as A};
